c8e1d37a99acef91fe7522ee87370b8bfead2114
Ryan T <ryan.turinsky@gmail.com>
2017-09-27 22:19:24 -0400

    Refactoring test class

--- src/test/java/com/ryan/kata/LineCommandParserTest.java
+++ src/test/java/com/ryan/kata/LineCommandParserTest.java
@@ -3,15 +3,21 @@ package com.ryan.kata;
 import com.ryan.kata.driver.Driver;
 import com.ryan.kata.parser.LineCommandParser;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class LineCommandParserTest {
 
+    private LineCommandParser lineCommandParser;
+
+    @Before
+    public void setup() {
+        lineCommandParser = new LineCommandParser();
+    }
+
     @Test
     public void parser_returns_Driver_when_first_command_is_Driver() {
         String driverLine = "Driver Peter";
-        LineCommandParser lineCommandParser = new LineCommandParser();
-
         Driver driver = lineCommandParser.parse(driverLine);
 
         Assert.assertNotNull(driver);
@@ -20,8 +26,6 @@ public class LineCommandParserTest {
     @Test
     public void parser_returns_Driver_with_proper_name() {
         String driverLine = "Driver Peter";
-        LineCommandParser lineCommandParser = new LineCommandParser();
-
         Driver driver = lineCommandParser.parse(driverLine);
 
         Assert.assertEquals("Peter", driver.getDriverName());
